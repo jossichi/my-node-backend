84ddc4ba88d38d49de5c21cab7e5b826c4f4f2ce
Jossichi <32387226+jossichi@users.noreply.github.com>
2024-08-28 07:56:40 +0700

    server.js

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 const axios = require('axios');
 const app = express();
 
-// Sử dụng biến môi trường PORT hoặc cổng mặc định 3001
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Route cho GET request tại đường dẫn gốc
+app.get('/', (req, res) => {
+  res.send('Welcome to the backend server!');
+});
 
 // Endpoint để nhận dữ liệu và gửi đến Google Sheets
 app.post('/submit', async (req, res) => {
   try {
-    // Gửi dữ liệu tới Google Apps Script
-    const response = await axios.post('https://script.google.com/macros/s/AKfycbyfMMwAq8O2L-qBRAsHJN-k8pRCEeRgmLL7Yl46XWE6-xx-a-knQH3MI5sE7PxZQWAnCw/exec', req.body);
+    const response = await axios.post(
+      'https://script.google.com/macros/s/AKfycbyfMMwAq8O2L-qBRAsHJN-k8pRCEeRgmLL7Yl46XWE6-xx-a-knQH3MI5sE7PxZQWAnCw/exec',
+      req.body
+    );
     res.status(response.status).send(response.data);
   } catch (error) {
-    console.error('Error occurred:', error.message); // Log lỗi chi tiết
+    console.error('Error occurred:', error);
     res.status(error.response?.status || 500).send(error.message);
   }
 });
