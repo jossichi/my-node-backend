d4daff242e8f868d755f7c8f620bcaa562d7b5cc
Jossichi <32387226+jossichi@users.noreply.github.com>
2024-08-28 12:26:54 +0700

    server.js

--- server.js
+++ server.js
@@ -5,22 +5,24 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Chuyển hướng từ '/' sang '/submit'
+// Route cho GET request tại đường dẫn gốc
 app.get('/', (req, res) => {
-  res.redirect('/submit');
+  res.send('Welcome to the backend server!');
 });
 
 // Endpoint để nhận dữ liệu và gửi đến Google Sheets
 app.post('/submit', async (req, res) => {
   try {
-    const response = await axios.post('https://script.google.com/macros/s/AKfycbyfMMwAq8O2L-qBRAsHJN-k8pRCEeRgmLL7Yl46XWE6-xx-a-knQH3MI5sE7PxZQWAnCw/exec', req.body);
+    const response = await axios.post(
+      'https://script.google.com/macros/s/AKfycbyfMMwAq8O2L-qBRAsHJN-k8pRCEeRgmLL7Yl46XWE6-xx-a-knQH3MI5sE7PxZQWAnCw/exec',
+      req.body
+    );
     res.status(response.status).send(response.data);
   } catch (error) {
-    console.error('Error occurred:', error.message);
+    console.error('Error occurred:', error);
     res.status(error.response?.status || 500).send(error.message);
   }
 });
